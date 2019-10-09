d0bcf4f9cb040aede767a50d5a27cc6b00418578
Tym <Tym@DESKTOP-IA4KDL4>
2019-10-09 11:13:39 -0700

    Second version

--- cse360assign2/AddingMachine.java
+++ cse360assign2/AddingMachine.java
@@ -1,31 +1,38 @@
-//Initial version
+//Second version
 package cse360assign2;
 
 public class AddingMachine {
 
 	private int total;
 	
+	private String history;
+	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
-	public int getTotal () {
-		return 0;
+	public int getTotal () { //Return the total
+		return total;
 	}
 	
-	public void add (int value) {
-		
+	public void add (int value) { //Add value to total and include it in the history
+		total += value;
+		history = history + " + " + Integer.toString(value);
 	}
 	
-	public void subtract (int value) {
-		
+	public void subtract (int value) { //Subtract value to total and include it in the history
+		total -= value;
+		history = history + " - " + Integer.toString(value);
 	}
 		
-	public String toString () {
-		return "";
+	public String toString () { //Return the history
+		return history;
 	}
 
-	public void clear() {
+	public void clear() { //Clear the history
+		total = 0;
+		history = "0";
 	
 	}
 }
